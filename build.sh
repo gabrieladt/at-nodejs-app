5a9996515bd739cbfddfa00d241d4740e7bd256b
Gabriel Ferreira <gabrieladt@gmail.com>
2015-04-22 18:52:59 -0300

    .

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 tag=`git rev-parse  --short HEAD`
-branch_name=`git ls-remote --heads origin | grep $(git rev-parse HEAD) | cut -d / -f 3`
+branch_name=`git ls-remote --heads origin | grep $(git rev-parse HEAD) | cut -d / -f 3 | head -n1`
 
 #ID=$(sudo docker build -t local/${branch_name} .)
 #sudo docker tag $ID local/${branch_name}:${tag}
