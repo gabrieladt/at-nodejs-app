9ae30d08140cc880d30d26e13ba90b92851d3446
Gabriel Ferreira <gabrieladt@gmail.com>
2015-04-22 16:38:06 -0300

    add dev_apps cookbook

--- dev_apps/recipes/deploy.rb
+++ dev_apps/recipes/deploy.rb
@@ -1,69 +1,26 @@
-include_recipe 'deploy'
-include_recipe 'dependencies'
-
-node[:deploy].each do |application, deploy|
-  opsworks_deploy_dir do
-    user deploy[:user]
-    group deploy[:group]
-  end
-
-  opsworks_deploy do
-    deploy_data deploy
-    app application
-  end
-end
-
-node[:dedalus_apps_containers].each do |name, image|
-	directory "/apps/#{name}/sites-enabled" do
-	        owner 'deploy'
-	        group 'root'
-	        mode '0775'
-	end
-	directory "/apps/#{name}/" do
-	        owner 'deploy'
-	        group 'root'
-	        mode '0775'
-	end
-	directory "/apps/#{name}/certs" do
-	        owner 'deploy'
-	        group 'root'
-	        mode '0775'
-	end
-	directory "/apps/#{name}/logs" do
-	        owner 'deploy'
-	        group 'root'
-	        mode '0775'
-	end
-	directory "/apps/#{name}/www" do
-	        owner 'deploy'
-	        group 'root'
-	        mode '0775'
-	end
-
-	script "deploy_app" do
+node[:apps_containers_dev].each do |name, image|
+	script "deploy_app_dev" do
 		interpreter "bash"
 		user "root"
 		code <<-EOH
-docker pull #{image}
-
 /usr/bin/docker.io ps -a | grep #{name} | grep Exited 
 
 if [ $? -eq 0 ]; then
         /usr/bin/docker.io stop #{name} 
         /usr/bin/docker.io rm #{name} 
-	docker run --name #{name} -d -p #{node[name][:docker_port]}:80 -v /apps/#{name}/sites-enabled:/etc/nginx/sites-enabled  -v /apps/#{name}/logs:/var/log/nginx -v /apps/#{name}:/apps/#{name}  #{image} 
+	docker run --name #{name} -d -p #{node[name][:docker_port]}:3000 #{image} 
 fi
 
 /usr/bin/docker.io ps -a | grep #{name}
 if [ $? -ne 0 ]; then
-        docker run --name #{name} -d -p #{node[name][:docker_port]}:80 -v /apps/#{name}/sites-enabled:/etc/nginx/sites-enabled -v /apps/#{name}/logs:/var/log/nginx -v /apps/#{name}:/apps/#{name}  #{image} 
+        docker run --name #{name} -d -p #{node[name][:docker_port]}:3000 #{image} 
 fi
 
 /usr/bin/docker.io ps -a | grep #{name} | grep -v "Up " 
 if [ $? -eq 0 ]; then
 	service docker.io restart
         /usr/bin/docker.io rm -f #{name} 
-        docker run --name #{name} -d -p #{node[name][:docker_port]}:80 -v /apps/#{name}/sites-enabled:/etc/nginx/sites-enabled -v /apps/#{name}/logs:/var/log/nginx -v /apps/#{name}:/apps/#{name}  #{image} 
+        docker run --name #{name} -d -p #{node[name][:docker_port]}:3000 #{image} 
 fi
 
 /usr/bin/docker.io ps -a | grep #{name} | awk '{print $1}'| while read i 
@@ -75,32 +32,11 @@ fi
         if [ "$RUNNING" != "$LATEST" ];then
                 /usr/bin/docker.io stop $i 
                 /usr/bin/docker.io rm $i 
-                docker run --name #{name} -d -p #{node[name][:docker_port]}:80 -v /apps/#{name}/sites-enabled:/etc/nginx/sites-enabled -v /apps/#{name}/logs:/var/log/nginx -v /apps/#{name}:/apps/#{name}  #{image} 
+        	docker run --name #{name} -d -p #{node[name][:docker_port]}:3000 #{image} 
         else
                 echo "#{name} up to date"
         fi
 done
 	EOH
 	end
-
-        execute "docker_hup_app" do
-                command "docker restart #{name}"
-                action [ :nothing ]
-        end
-
-        template "/apps/#{name}/sites-enabled/#{name}.conf" do
-                source "nginx_vhost.conf.erb"
-                mode 0755
-                group 'root'
-                owner 'root'
-		 variables(
-                        :app_name => "#{name}"
-                )
-                notifies :run, "execute[docker_hup_app]", :immediately
-        end
-	
-	execute "docker_app_restart" do
-                command "docker restart #{name}"
-                action [ :run ]
-        end
 end
